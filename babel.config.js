4cdaafb656d833556d98bd98199c5fee6da3b614
Mashfiqur Rahman <shareable.resources@gmail.com>
2024-10-14 15:39:16 +1100

    config update

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'], // Use React Native's Babel preset
 };
